df1c3d673ab9245630851befc1e64175c9c0cac2
lexa <sokolov@ozero.net>
2018-04-25 23:12:14 +0300

    Fields and constants added

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,8 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Hangman {
     String word;
     StringBuilder currentGuess;
     ArrayList<Character> previousGuesses = new ArrayList<Character>();
+
+    int maxTries = 6;
+    int currentTry = 0;
+
+    ArrayList<String> dictonary = new ArrayList<String>();
+    private static FileReader fileReader;
+    private static BufferedReader bufferedFileReader;
 
 }
