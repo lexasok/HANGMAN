7438fd6988174969b4e9d016c8b772d3aa631d81
lexa <sokolov@ozero.net>
2018-04-25 22:59:18 +0300

    Main cycle created

--- src/main/java/HangmanApp.java
+++ src/main/java/HangmanApp.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class HangmanApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("It's f@cking hangman, man!");
+        System.out.println();
 
+        boolean doYouWantToPlay = true;
+
+        while (doYouWantToPlay) {
+            System.out.println();
+        }
     }
 }
